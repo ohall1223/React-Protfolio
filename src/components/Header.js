e880c3afc79508fe81403b2a63eb67d71ecf7a87
Olivia Hall <ohall1223@gmail.com>
2022-01-26 16:59:31 -0700

    App.js worked on & the header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,3 +5,25 @@ import About from "../components/About";
 import Portfolio from "../components/Portfolio";
 import ContactForm from "../components/ContactForm"
 import Resume from "../components/Resume"
+
+class Header extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <header>
+                    <Navigation></Navigation>
+                </header>
+
+                <div className="content">
+                    <Route exact path="/" render={() => <Redirect to="/about" />} />
+                    <Route path="/about" component={About} />
+                    <Route path="/portfolio" component={Portfolio} /> 
+                    <Route path="/contact" component={ContactForm} />
+                    <Route path="/resume" component={Resume} />
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default Header
