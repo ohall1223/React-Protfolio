74d8c5f6c538d053f873f4faa186ef9075372565
Olivia Hall <ohall1223@gmail.com>
2022-01-25 18:24:03 -0700

    about

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return(
+        <section className="container">
+            <h2 class="top-title">Olivia Hall</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="https://avatars.githubusercontent.com/u/81581516?v=4" alt="Olivia Hall"></img>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import { Route, Redirect, HashRouter } from "react-router-dom";
+import Navigation from '../components/Navigation';
+import About from "../components/About";
