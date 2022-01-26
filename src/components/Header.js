1550d0354fdb5ba4af366480a0569596f2a00793
Olivia Hall <ohall1223@gmail.com>
2022-01-25 18:59:27 -0700

    resume section worked on

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,3 +3,5 @@ import { Route, Redirect, HashRouter } from "react-router-dom";
 import Navigation from '../components/Navigation';
 import About from "../components/About";
 import Portfolio from "../components/Portfolio";
+import ContactForm from "../components/ContactForm"
+import Resume from "../components/Resume"
---
+++ src/components/Resume.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section class="container">
+            <h2 className="top-title">Resume</h2>
+            <hr></hr>
+            <div>
+                <div class="mt-5">
+                    <h2 className="top-title">Olivia Hall</h2>
+                    <ul>
+                        <h4>Qualifications:</h4>
+                        <li>
+                            Like hands-on work within a team environment. Good at resolving problems and thinking logically, analytically, and creatively. Proficient with computing programs such as SolidWorks, MATLAB, AutoCAD, Mathcad, RAM Steel, and ProSteel , EPA Net, Microsoft Word and Excel, EnerCalc, RetainPro. Comfortable with writing, concisely and professionally.
+                        </li>
+                        <h4>Education:</h4>
+                        <li>
+                            2008-2013 University of Colorado at Denver School of Engineering and Applied Sciences Major: Engineering-Civil Engineering GPA 3.30
+
+                            2016 Red Rocks Community College Civil 3D drafting course
+                        </li>
+                        <h4>Experience:</h4>
+                        <li>
+                            8/19-12/19 Lead Drafting Engineer, Elite Environmental Controls
+                            Responsible for drafting environmental control plans for the cannabis industry. My responsibilities included: Drafting CO2 lines from the tank, to the safety valve, to the flow box, which distributed CO2 to the various, grow rooms. Drafting wiring plans including sensor & alarm schematics and wiring from CO2 , temperature and humidity sensors to the environmental control panel to the CO2 alarms. Drafting safety valve schematics, control panel schematics & flow box schematics. Thoroughly researching the CO2 enrichment building codes in Colorado, California, Michigan, Massachusetts & Maryland.
+
+                            10/17-12/18 Lead Drafting Engineer, Universal Grow Controls
+                            Responsible for drafting carbon dioxide enrichment and irrigation plans for the cannabis industry. My responsibilities included: Drafting CO2 lines from the tank, to the safety valve, to the flow box, which distributed CO2 to the various, grow rooms. Drafting wiring plans including sensor & alarm schematics and wiring from the CO2 sensors to the control panel to the CO2 alarms. Drafting safety valve schematics, control panel schematics & flow box schematics. Thoroughly researching the CO2 enrichment building codes in Colorado, California, Michigan, Massachusetts & Maryland. Drafting piping and instrumentation diagrams for irrigation systems including various methods of dosing water with nutrients.
+
+                            05/14-06/15 Engineering Consultant, Alliance Engineering LTD
+                            Responsible for consulting and design of several civil engineering disciplines including Foundation design for residential buildings Parcel layout for subdivisions Beam design for residential and commercial buildings Flood plain development permits, including flood plain cross sections Water and waste-water system layouts Retaining wall and flood wall design UDFCD Drainage Studies and Detention Basin Sizing
+
+                            10/13-05/14 E.I., Tower Engineering Professionals, Inspections team
+                            Responsible for inspecting towers and drafting reports for clients Performing inspections involves climbing towers, taking measurements, notes and photos of essential elements for later review Responsible for drafting reports, based on notes and photos taken during inspections, for clients on AutoCAD pertaining to cellular towers foundations, structures, mounts and antennas
+
+                            4/12-8/12 Intern, CH2M Hill, Energy & Chemicals Structural team
+                            Performed several tasks for engineers, calculation and administrative work for mining and refinery projects Became proficient at several engineering design programs (noted above) Gained experience in the process and management of taking a project from bid, to design, to manufacturing/construction
+                        </li>
+                        <h4>Certifications and Honors:</h4>
+                        <li>
+                            08/13 Outstanding Undergraduate Student in Civil Engineering (Faculty Nominated)
+
+                            12/12 Fundamentals of Engineering certification exam
+
+                            11/11 ACI Concrete Field Testing Technician- Grade 1
+                        </li>
+                        <h4>Interests:</h4>
+                        <li>
+                            Sustainability, green space, efficient homes and buildings Making a difference in how the world views modern civilization
+                        </li>
+                        <h4>References:</h4>
+                        <li>
+                            Available upon request
+                        </li>
+                    </ul>
+                    <p class="mt-5">
+                        <a href="https://www.linkedin.com/in/olivia-hall-ei-a524b364/">
+                            <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="linkedIn">
+                            </img>
+                        </a>
+                    </p>
+                    <a href="https://github.com/ohall1223" alt="github">
+
+                    </a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Resume;
