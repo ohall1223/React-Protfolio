1f4f53ec100e2cb09b6ac947d4b967b5665b6242
Olivia Hall <ohall1223@gmail.com>
2022-01-26 18:30:54 -0700

    Portfolio woked

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProjectCards from "../components/Project"
-import portfolio from '../portolio.json';
+import portfolio from './portfolio.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Wrapper (props) {
