274e865c2cd8f3219d706c52cc17fb75635cd102
Olivia Hall <ohall1223@gmail.com>
2022-02-01 17:49:33 -0700

    more stuuff

---
+++ build/precache-manifest.82c4add7cfac04233dbea5dc8046713e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f59459669a7c10c6f8de7021e0a3bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78e0b9edfae05ba24614",
+    "url": "/static/css/2.cad033e0.chunk.css"
+  },
+  {
+    "revision": "45e44659f3f9836d98cf",
+    "url": "/static/css/main.dce20561.chunk.css"
+  },
+  {
+    "revision": "78e0b9edfae05ba24614",
+    "url": "/static/js/2.35f4dda1.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.35f4dda1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf57662744c3cb8105c",
+    "url": "/static/js/3.c1280d43.chunk.js"
+  },
+  {
+    "revision": "45e44659f3f9836d98cf",
+    "url": "/static/js/main.5ea012e8.chunk.js"
+  },
+  {
+    "revision": "28a948feeb2d9b4884c5",
+    "url": "/static/js/runtime-main.644c2349.js"
+  }
+]);
