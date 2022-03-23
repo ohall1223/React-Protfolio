a9eb28a14ac735c536c20373ec623c02fbd2e947
Olivia Hall <ohall1223@gmail.com>
2022-03-23 14:39:32 -0600

    formatting resumes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -16,50 +16,63 @@ function Resume() {
           </a>
     
                         <h3>Qualifications:</h3>
-                        <h4>
-                            Like hands-on work within a team environment. Good at resolving problems and thinking logically, analytically, and creatively. Proficient with programming languages such as CSS, HTML, JavaScript, JSON, MySQL, MongoDB, Express.js, and React.js. Proficient with computing programs such as SolidWorks, MATLAB, AutoCAD, Mathcad, RAM Steel, and ProSteel , EPA Net, Microsoft Word and Excel, EnerCalc, RetainPro. Comfortable with writing, concisely and professionally.
-                        </h4>
-                        {/* <h4>Education:</h4>
-                        <li>
-                        2021-2022 University of Denver Full Stack Coding Bootcamp
 
-                        2016 Red Rocks Community College Civil 3D drafting course
+                            <h5>Like hands-on work within a team environment.</h5>
+
+                            <h5>Good at resolving problems and thinking logically, analytically, and creatively.</h5>
+
+                            <h5>Proficient with programming languages such as CSS, HTML, JavaScript, JSON, MySQL, MongoDB, Express.js, and React.js.</h5>
+
+                            <h5>Proficient with computing programs such as SolidWorks, MATLAB, AutoCAD, Mathcad, RAM Steel, and ProSteel , EPA Net, Microsoft Word and Excel, EnerCalc, RetainPro. Comfortable with writing, concisely and professionally.</h5>
+
+                         <h3>Education:</h3>
+
+                            <h5>2021-2022 University of Denver</h5> 
+                                <p>Full Stack Coding Bootcamp</p>
+
+                            <h5>2016 Red Rocks Community College</h5> 
+                                <p>Civil 3D Drafting Course</p>
                         
-                        2008-2013 University of Colorado at Denver School of Engineering and Applied Sciences Major: Engineering-Civil Engineering GPA 3.30
-                        </li>
-                        <h4>Experience:</h4>
-                        <li>
-                            8/19-12/19 Lead Drafting Engineer, Elite Environmental Controls
-                            Responsible for drafting environmental control plans for the cannabis industry. My responsibilities included: Drafting CO2 lines from the tank, to the safety valve, to the flow box, which distributed CO2 to the various, grow rooms. Drafting wiring plans including sensor & alarm schematics and wiring from CO2 , temperature and humidity sensors to the environmental control panel to the CO2 alarms. Drafting safety valve schematics, control panel schematics & flow box schematics. Thoroughly researching the CO2 enrichment building codes in Colorado, California, Michigan, Massachusetts & Maryland.
+                            <h5>2008-2013 University of Colorado at Denver</h5> 
+                                <p>School of Engineering and Applied Sciences Major: Engineering-Civil Engineering GPA 3.30</p>
 
-                            10/17-12/18 Lead Drafting Engineer, Universal Grow Controls
-                            Responsible for drafting carbon dioxide enrichment and irrigation plans for the cannabis industry. My responsibilities included: Drafting CO2 lines from the tank, to the safety valve, to the flow box, which distributed CO2 to the various, grow rooms. Drafting wiring plans including sensor & alarm schematics and wiring from the CO2 sensors to the control panel to the CO2 alarms. Drafting safety valve schematics, control panel schematics & flow box schematics. Thoroughly researching the CO2 enrichment building codes in Colorado, California, Michigan, Massachusetts & Maryland. Drafting piping and instrumentation diagrams for irrigation systems including various methods of dosing water with nutrients.
+                        <h3>Experience:</h3>
+                        
+                            <h5>8/19-12/19 Lead Drafting Engineer, Elite Environmental Controls</h5>
+                                <p>Responsible for drafting environmental control plans for the cannabis industry. My responsibilities included: Drafting CO2 lines from the tank, to the safety valve, to the flow box, which distributed CO2 to the various, grow rooms. Drafting wiring plans including sensor & alarm schematics and wiring from CO2 , temperature and humidity sensors to the environmental control panel to the CO2 alarms. Drafting safety valve schematics, control panel schematics & flow box schematics. Thoroughly researching the CO2 enrichment building codes in Colorado, California, Michigan, Massachusetts & Maryland.</p>
+                            
 
-                            05/14-06/15 Engineering Consultant, Alliance Engineering LTD
-                            Responsible for consulting and design of several civil engineering disciplines including Foundation design for residential buildings Parcel layout for subdivisions Beam design for residential and commercial buildings Flood plain development permits, including flood plain cross sections Water and waste-water system layouts Retaining wall and flood wall design UDFCD Drainage Studies and Detention Basin Sizing
+                            <h5>10/17-12/18 Lead Drafting Engineer, Universal Grow Controls</h5>
+                                <p>Responsible for drafting carbon dioxide enrichment and irrigation plans for the cannabis industry. My responsibilities included: Drafting CO2 lines from the tank, to the safety valve, to the flow box, which distributed CO2 to the various, grow rooms. Drafting wiring plans including sensor & alarm schematics and wiring from the CO2 sensors to the control panel to the CO2 alarms. Drafting safety valve schematics, control panel schematics & flow box schematics. Thoroughly researching the CO2 enrichment building codes in Colorado, California, Michigan, Massachusetts & Maryland. Drafting piping and instrumentation diagrams for irrigation systems including various methods of dosing water with nutrients.</p>
 
-                            10/13-05/14 E.I., Tower Engineering Professionals, Inspections team
-                            Responsible for inspecting towers and drafting reports for clients Performing inspections involves climbing towers, taking measurements, notes and photos of essential elements for later review Responsible for drafting reports, based on notes and photos taken during inspections, for clients on AutoCAD pertaining to cellular towers foundations, structures, mounts and antennas
+                            <h5>05/14-06/15 Engineering Consultant, Alliance Engineering LTD</h5>
+                                <p>Responsible for consulting and design of several civil engineering disciplines including Foundation design for residential buildings Parcel layout for subdivisions Beam design for residential and commercial buildings Flood plain development permits, including flood plain cross sections Water and waste-water system layouts Retaining wall and flood wall design UDFCD Drainage Studies and Detention Basin Sizing</p>
 
-                            4/12-8/12 Intern, CH2M Hill, Energy & Chemicals Structural team
-                            Performed several tasks for engineers, calculation and administrative work for mining and refinery projects Became proficient at several engineering design programs (noted above) Gained experience in the process and management of taking a project from bid, to design, to manufacturing/construction
-                        </li>
+                            <h5>10/13-05/14 E.I., Tower Engineering Professionals, Inspections team</h5>
+                                <p>Responsible for inspecting towers and drafting reports for clients Performing inspections involves climbing towers, taking measurements, notes and photos of essential elements for later review Responsible for drafting reports, based on notes and photos taken during inspections, for clients on AutoCAD pertaining to cellular towers foundations, structures, mounts and antennas</p>
+
+                            <h5>4/12-8/12 Intern, CH2M Hill, Energy & Chemicals Structural team</h5>
+                                <p>Performed several tasks for engineers, calculation and administrative work for mining and refinery projects Became proficient at several engineering design programs (noted above) Gained experience in the process and management of taking a project from bid, to design, to manufacturing/construction</p>
+    
                         <h4>Certifications and Honors:</h4>
-                        <li>
-                            08/13 Outstanding Undergraduate Student in Civil Engineering (Faculty Nominated)
 
-                            12/12 Fundamentals of Engineering certification exam
+                        <li>08/13 Outstanding Undergraduate Student in Civil Engineering (Faculty Nominated)</li>
 
-                            11/11 ACI Concrete Field Testing Technician- Grade 1
-                        </li>
+                        <li>12/12 Fundamentals of Engineering certification exam</li>
+
+                        <li>11/11 ACI Concrete Field Testing Technician- Grade 1</li>
+
                         <h4>Interests:</h4>
+
                         <li>
                             Sustainability, green space, efficient homes and buildings Making a difference in how the world views modern civilization
                         </li>
+
                         <h4>References:</h4>
+
                         <li>
                             Available upon request
-                        </li> */}
+                        </li> 
                     <p class="mt-5">
                         <a href="https://www.linkedin.com/in/olivia-hall-ei-a524b364/">
                             <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="linkedIn">
